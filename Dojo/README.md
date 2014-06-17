a64f3de001e4a08f132ebe66c48685cb40b2dddf
saml <skynare@gmail.com>
2014-06-17 17:05:36 -0400

    concurrent knn.py

---
+++ Dojo/README.md
@@ -0,0 +1,13 @@
+# python
+
+    $ python knn.py --concurrent 6
+    95.4% Took: 0:00:37.439134
+    python knn.py --concurrent 6  211.48s user 0.62s system 542% cpu 39.108 total
+    
+# rust
+
+    $ rustc -O knn.rs
+    $ ./knn
+    Percentage correct: 94.4% Took: 0.754412
+    ./knn  4.48s user 0.06s system 486% cpu 0.934 total
+    
--- Dojo/knn.py
+++ Dojo/knn.py
@@ -2,6 +2,7 @@ import csv
 import argparse
 import datetime
 import sys
+import multiprocessing
 
 def distance_sqr(xs, ys):
     result = 0
@@ -28,20 +29,42 @@ def find_match(valid_pixels, train_samples):
 
     return result
 
+class FindMatch(object):
+    def __init__(self, validation_samples):
+        self.validation_samples = validation_samples
+
+    def __call__(self, data):
+        answer = find_match(data['pixels'], self.validation_samples)
+        if data['label'] == answer['label']:
+            return 1
+        return 0
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--train', default='trainingsample.csv', help='train sample csv [%(default)s]')
     parser.add_argument('--validation', default='validationsample.csv', help='validation csv [%(default)s]')
+    parser.add_argument('--concurrent', type=int, default=1, help='number of parallel processes [%(default)s]')
     args = parser.parse_args()
 
     train_samples = load_data(args.train)
     validation_samples = load_data(args.validation)
 
+    buff = []
+    sample_count = len(train_samples)
+    split_count = sample_count / args.concurrent
+    worker = FindMatch(validation_samples)
+    pool = multiprocessing.Pool(processes=args.concurrent)
+
     t = datetime.datetime.now()
     match_count = 0
-    for train_data in train_samples:
-        answer = find_match(train_data['pixels'], validation_samples)
-        if train_data['label'] == answer['label']:
-            match_count += 1
+
+    for i,train_data in enumerate(train_samples, 1):
+        buff.append(train_data)
+        if i % split_count == 0:
+            match_count += sum(pool.map(worker, buff))
+            buff = []
+    
+    if len(buff) > 0:
+        match_count += sum(pool.map(worker, buff))
     
     print('%s%% Took: %s' % (float(match_count)/len(validation_samples)*100, datetime.datetime.now() - t))
