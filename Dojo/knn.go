a24cf4bd4017e7a2b6b8ef867c1593762ed41889
saml <skynare@gmail.com>
2014-06-20 15:31:30 -0400

    parallel go

--- Dojo/knn.go
+++ Dojo/knn.go
@@ -1,74 +1,64 @@
 package main
- 
+
 import (
 	"encoding/csv"
 	"fmt"
 	"io"
 	"log"
 	"os"
+	"runtime"
 	"strconv"
 	"time"
 )
- 
+
 func readData(filename string) [][]int {
 	var rows [][]int
- 
+
 	file, err := os.Open(filename)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
- 
+
 	c := csv.NewReader(file)
- 
+
 	// skip header
 	c.Read()
- 
+
 	for {
-        data := []int{}
+		data := []int{}
 		row, err := c.Read()
 		if err == io.EOF {
 			break
 		} else if err != nil {
 			log.Fatal(err)
 		}
- 
+
 		// convert elements to int
 		for i := 0; i < len(row); i++ {
 			v, _ := strconv.Atoi(row[i])
 			data = append(data, v)
 		}
- 
+
 		rows = append(rows, data)
 	}
- 
+
 	return rows
 }
- 
-func main() {
-	train := readData("trainingsample.csv")
-	valid := readData("validationsample.csv")
-	start := time.Now()
- 
-	var (
-		match    int
-		minSum   int
-		minLabel int
-		dist     int
-		xsp      []int
-		ysp      []int
-		diff     int
-	)
- 
-	for _, xs := range valid {
-		minSum = 0
-		minLabel = 0
-		for _, ys := range train {
-			dist = 0
-			xsp = xs[1:]
-			ysp = ys[1:]
+
+func calcGoodCount(begin int, end int, images [][]int, samples [][]int, c chan int) {
+	match := 0
+
+	for i := begin; i < end; i++ {
+		xs := images[i]
+		minSum := 0
+		minLabel := 0
+		for _, ys := range samples {
+			dist := 0
+			xsp := xs[1:]
+			ysp := ys[1:]
 			for i := 0; i < len(xsp); i++ {
-				diff = xsp[i] - ysp[i]
+				diff := xsp[i] - ysp[i]
 				dist += diff * diff
 			}
 			if minSum == 0 || dist < minSum {
@@ -80,7 +70,33 @@ func main() {
 			match++
 		}
 	}
- 
-	fmt.Printf("Match: %v%% \n", float64(match*100)/float64(len(valid)))
+
+	c <- match
+}
+
+func main() {
+	train := readData("trainingsample.csv")
+	valid := readData("validationsample.csv")
+
+	numCpu := runtime.NumCPU()
+	runtime.GOMAXPROCS(numCpu)
+
+	start := time.Now()
+
+	inputSize := len(valid)
+	chunkSize := inputSize / numCpu
+	c := make(chan int, numCpu)
+
+	for i := 0; i < numCpu; i++ {
+		go calcGoodCount(i*chunkSize, (i+1)*chunkSize, valid, train, c)
+	}
+
+	match := 0
+	for i := 0; i < numCpu; i++ {
+		matches := <-c
+		match += matches
+	}
+
+	fmt.Printf("Match: %d %.2f%% \n", match, float64(match*100)/float64(len(valid)))
 	fmt.Printf("Duration: %s \n", time.Since(start).String())
 }
